e6474e655447742bea0f69a36b456f620de1d1d0
nusrname <zhelezniyche@yandex.ru>
2025-04-13 18:30:16 +0400

    Отформатирован код с помощью clang-format и уменьшен размер текстуры противника

--- user.cpp
+++ user.cpp
@@ -212,12 +212,12 @@ bool CheckFinish(Object &player, Scene &scene) {
 // Возможное решение может занимать примерно 16-20 строк.
 // Ваше решение может сильно отличаться.
 //
-void EnemyAI(Object &enemy, Scene &scene, float dt)
-{
+void EnemyAI(Object &enemy, Scene &scene, float dt) {
     Object *player = find_player(scene);
 
-    if (!player)
+    if (!player) {
         return;
+    }
 
     float dx = enemy.position.x - player->position.x;
     float move = enemy.enemy.speed * dt;
